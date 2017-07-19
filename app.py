3b13ed4e550f54cbea6095b02bfccb80a04d6397
kaliaksander <k.aliaksander@gmail.com>
2017-07-20 01:33:24 +0300

    commit_4_6

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, redirect, url_for ,request, session, flash
+from flask import Flask, render_template, redirect, url_for ,request, session, flash, g
 from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
+app.database = "sample.db"
 
 app.secret_key='my previous'
 
@@ -19,7 +21,14 @@ def login_required(f):
 @login_required
 def home():
     #return "Hello, world!"
-    return render_template('index.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = []
+    for row in cur.fetchall():
+        posts.append(dict(title=row[0], description=row[1]))
+    #posts = [dict(title = row[0],description = row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html',posts=posts)
 
 @app.route('/welcome')
 def welcome():
@@ -44,6 +53,9 @@ def logout():
     session.pop('logged_in',None)
     flash('You were just logged out!')
     return redirect(url_for('welcome'))
+
+def connect_db():
+    return sqlite3.connect(app.database)
 
 if __name__=='__main__':
     app.run(debug=True)

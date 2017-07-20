39595730e3cf747e2e29ef316f64b8f79d23db63
kaliaksander <k.aliaksander@gmail.com>
2017-07-20 14:18:50 +0300

    try-except

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from functools import wraps
 import sqlite3
 
 app = Flask(__name__)
-app.database = "sample.db"
+app.database = "samle.db"
 
 app.secret_key='my previous'
 
@@ -21,13 +21,16 @@ def login_required(f):
 @login_required
 def home():
     #return "Hello, world!"
-    g.db = connect_db()
-    cur = g.db.execute('select * from posts')
     posts = []
-    for row in cur.fetchall():
-        posts.append(dict(title=row[0], description=row[1]))
-    #posts = [dict(title = row[0],description = row[1]) for row in cur.fetchall()]
-    g.db.close()
+    try:
+        g.db = connect_db()
+        cur = g.db.execute('select * from posts')
+        for row in cur.fetchall():
+            posts.append(dict(title=row[0], description=row[1]))
+        #posts = [dict(title = row[0],description = row[1]) for row in cur.fetchall()]
+        g.db.close()
+    except sqlite3.OperationalError:
+        flash("You have no database")
     return render_template('index.html',posts=posts)
 
 @app.route('/welcome')

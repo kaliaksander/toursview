f8d86445a832d911d79846ba76189f1c34cfdffb
kaliaksander <k.aliaksander@gmail.com>
2017-07-25 01:19:09 +0300

    new app

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, redirect, url_for ,request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
-# import sqlite3
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField
+from wtforms.validators import input_required,email,length
+from flask_bootstrap import Bootstrap
+#import sqlite3
 
 app = Flask(__name__)
-# app.database = "samle.db"
+Bootstrap(app)
 
+class LoginForm(FlaskForm):
+    username = StringField('username',validators=[input_required(),length(min=4,max=15)])
+    password = PasswordField('password',validators=[input_required(),length(min=8,max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email',validators=[input_required(),email(message='invalid email'),length(max=50)])
+    username = StringField('username', validators=[input_required(), length(min=4, max=15)])
+    password = PasswordField('password', validators=[input_required(), length(min=8, max=80)])
 # config
 app.config.from_object('config.DevelopmentConfig')
 
@@ -14,45 +27,28 @@ db = SQLAlchemy(app)
 
 from models import *
 
-
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args,**kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-
 @app.route('/')
-@login_required
-def home():
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html',posts=posts)
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            flash('You were just logged in!')
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+    form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>'+form.username.data + ' ' + form.password.data + '</h1>'
 
-@app.route('/logout')
-@login_required
-def logout():
-    session.pop('logged_in',None)
-    flash('You were just logged out!')
-    return redirect(url_for('welcome'))
+    return render_template('login.html',form=form)
+
+@app.route('/signup',methods=['GET','POST'])
+def  signup():
+    form=RegisterForm()
+    if form.validate_on_submit():
+        return '<h1>'+form.username.data + ' ' + form.email.data + ' ' + form.password.data +  '</h1>'
+    return render_template('signup.html',form=form)
+
+@app.route('/dashboard')
+def welcome():
+    return render_template('dashboard.html')
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)

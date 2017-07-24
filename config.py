8313467b45413db3b1ec487b8e2c27e1ce8d15b9
kaliaksander <k.aliaksander@gmail.com>
2017-07-24 15:04:52 +0300

    configuration commit1

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = 'y\xfe\x0e\x92\xbd\xa8$\xcb\x18j\xe1\xe82\x10A\xf9T\xd2\x87\xda,\x95\x8e\xad'
     # local version
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
     # heroku version
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True

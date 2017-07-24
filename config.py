17f55246758271cf9c5572e15baab604e2a12596
kaliaksander <k.aliaksander@gmail.com>
2017-07-24 14:46:18 +0300

    added psycopg2

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import os
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = 'y\xfe\x0e\x92\xbd\xa8$\xcb\x18j\xe1\xe82\x10A\xf9T\xd2\x87\xda,\x95\x8e\xad'
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
+    # local version
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
+    # heroku version
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
